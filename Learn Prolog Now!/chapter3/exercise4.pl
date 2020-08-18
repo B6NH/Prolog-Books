4cef7d538d53b7faef3d4aab9c0e76d853524d02
Jan Kowalski <yakov85@o2.pl>
2020-08-18 12:53:23 +0200

    Tree

--- Learn Prolog Now!/chapter3/exercise4.pl
+++ Learn Prolog Now!/chapter3/exercise4.pl
@@ -1,1 +1,5 @@
 /* Exercise 4 */
+
+greater_than(succ(_),0).
+greater_than(succ(A),succ(B)) :-
+  greater_than(A,B).
--- Learn Prolog Now!/chapter3/exercise5.pl
+++ Learn Prolog Now!/chapter3/exercise5.pl
@@ -1,1 +1,9 @@
 /* Exercise 5 */
+
+/* tree(tree(leaf(1),leaf(2)),leaf(4)) */
+
+swap(leaf(A),leaf(A)).
+
+swap(tree(A,B),tree(C,D)) :-
+  swap(A,D),
+  swap(B,C).
